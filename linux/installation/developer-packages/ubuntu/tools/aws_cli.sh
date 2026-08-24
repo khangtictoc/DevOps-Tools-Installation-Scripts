de3959bb854098ad82af61baa4f74dee6370144d
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2026-08-25 02:19:18 +0700

    update: installation aws ssm plugins script

--- linux/installation/developer-packages/ubuntu/tools/aws_cli.sh
+++ linux/installation/developer-packages/ubuntu/tools/aws_cli.sh
@@ -23,6 +23,27 @@ detect_aws_url() {
     echo "https://awscli.amazonaws.com/awscli-exe-${aws_os}-${aws_arch}.zip"
 }
 
+
+# --- AWS Plugins ---------------------------------------
+
+
+install_ssm() {
+    echo "[INSTALLING PLUGIN] SSM (Session Manager)"
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+    sudo dpkg -i session-manager-plugin.deb
+
+    if ! command -v session-manager-plugin &>/dev/null; then
+        echo "[FAIL ❌] SSM Plugin installation failed!"
+        exit 1
+    else
+        echo "[CHECKED ✅] SSM Plugin command installed!"
+    fi
+}
+
+install_plugin() {
+    install_ssm
+}
+
 if ! command -v aws &>/dev/null; then
     echo "[INSTALLING ⬇️] AWS CLI"
     if [ "$OS" = "darwin" ]; then
@@ -35,7 +56,7 @@ if ! command -v aws &>/dev/null; then
         URL=$(detect_aws_url)
 
         curl --retry 3 --retry-delay 5 --connect-timeout 30 --max-time 120 -fsSL "$URL" -o awscliv2.zip
-        unzip awscliv2.zip
+        unzip -q awscliv2.zip
         sudo ./aws/install
         clean_up
     fi
@@ -49,3 +70,5 @@ if ! command -v aws &>/dev/null; then
 else
     echo "[CHECKED ✅] aws command exists!"
 fi
+
+install_plugin
